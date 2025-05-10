6a3573874014d3ce08f8e2f91285980578a18bc3
Sam-H06 <28872465@students.lincoln.ac.uk>
2025-05-11 00:16:05 +0100

    Update Item.cs

--- Item/Item.cs
+++ Item/Item.cs
@@ -1,21 +1,47 @@
-namespace DungeonExplorer.Item {
-    public abstract class Item {
-        private static int nextId = 1;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        public string Name { get; }
-        public string Description { get; }
-        public int Id { get; }
-        public bool Useable { get; }
+namespace DungeonExplorer
+{
+    public class Inventory
+    //class to mange more than 1 item objects
+    {
+        private List<Item> items = new List<Item>();
 
-        protected Item(string name, string description, bool useable)
+        public void AddItem(Item item)
+        //methord that addes item to invantory
         {
-            Name = name;
-            Description = description;
-            Useable = useable;
-            Id = nextId++;
+            items.Add(item);
+            Console.WriteLine($"{item.Name} is added to inventory!");
         }
 
-        public abstract void Use(Player.Player player);
-        // Planning public abstract void Use(Mob.Mob mob);
+        public List<Item> GetWeapons()
+        // use LINQ to get all weapons in inventory
+
+        {
+            return items.Where(item => item is Weapon).ToList();
+        }
+
+        public List<Item> GetPotions()
+        //LINQ and Lambda used to return all potion items
+        {
+            return items.Where(item => item is Potion).ToList();
+            // filters and returns items that are potions
+
+        }
+
+        public void ShowInventory()
+        // displays the names of all items in the inventory
+
+        {
+            Console.WriteLine("Inventory:");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
     }
 }
